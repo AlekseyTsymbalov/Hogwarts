74005d7009ad2f09f0be96cd85608230766910b2
Aleksey_Tsy <AlekseyTsymbalov@yahoo.com>
2025-05-05 20:55:05 +0300

    [Task_6] - Изменил данные поджключения к БД. Вывел пользователя.

--- src/include/constants.php
+++ src/include/constants.php
@@ -1,7 +1,7 @@
 <?php
 
-const DB_HOST = "db";
-const DB_PORT = "5432";
+const DB_HOST = "localhost";
+const DB_PORT = "5499";
 const DB_NAME = "Hogwarts_db";
 const DB_USER = "aleksey";
 const DB_PASSWORD = "magic127";
