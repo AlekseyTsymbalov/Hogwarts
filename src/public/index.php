d9e9c9511bdb98a17df97403d09addb9bb9d3a67
Aleksey_Tsy <AlekseyTsymbalov@yahoo.com>
2025-05-06 21:10:44 +0300

    [Task_7] - Добавил пока что обработку запроса в php

--- src/public/index.php
+++ src/public/index.php
@@ -8,6 +8,23 @@ require_once __DIR__ . '/../config/db.php';
 
 try {
     $pdo = getDB();
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $firstName = htmlspecialchars($_POST['first_name']);
+        $secondName = htmlspecialchars($_POST['second_name']);
+        $middleName = htmlspecialchars($_POST['middle_name']);
+        $dateOfBirth = htmlspecialchars($_POST['date_of_birth']);
+        $password = htmlspecialchars($_POST['password']);
+
+        $stmt = $pdo->prepare(
+            "INSERT INTO users (first_name, second_name, middle_name, date_of_birth, password) 
+        VALUES (:first_name, :second_name, :middle_name, :date_of_birth, :password)"
+        );
+        $stmt->execute([$firstName, $secondName, $middleName, $dateOfBirth, $password]);
+
+        echo '<p class="success">Пользователь добавлен!</p>';
+    }
+
     $stmt = $pdo->query("SELECT 1");
 } catch (PDOException $e) {
     echo "<h1>Ошибка подключения: </h1>";
@@ -42,11 +59,11 @@ try {
 <h1>Добро пожаловать в Hogwarts!</h1>
 <p>Здесь начинаются ваши знания.</p>
 <?php
-    $query = $pdo->query("SELECT * FROM users");
-    while ($row = $query->fetch()) {
-       echo sprintf("Имя: %s<br>", $row['first_name']);
-       echo sprintf("Фамилия: %s<br>", $row['second_name']);
-    }
+$query = $pdo->query("SELECT * FROM users");
+while ($row = $query->fetch()) {
+    echo sprintf("Имя: %s<br>", $row['first_name']);
+    echo sprintf("Фамилия: %s<br>", $row['second_name']);
+}
 ?>
 
 </body>
