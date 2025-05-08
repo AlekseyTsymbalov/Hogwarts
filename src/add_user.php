8bd60b796b8b3472df9ac0ef1617dcc8bf3a0382
Aleksey_Tsy <AlekseyTsymbalov@yahoo.com>
2025-05-08 21:56:51 +0300

    [Task_9] - Добавил поле email добавили проверки

--- src/add_user.php
+++ src/add_user.php
@@ -9,28 +9,34 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     die("Не допустимый HTTP метод!");
 }
 
-$firstName = htmlspecialchars($_POST['first_name']);
-$secondName = htmlspecialchars($_POST['second_name']);
-$middleName = htmlspecialchars($_POST['middle_name']);
-$dateOfBirth = htmlspecialchars($_POST['date_of_birth']);
-$password = htmlspecialchars($_POST['password']);
+$firstName = htmlspecialchars($_POST['first_name'] ?? "");
+$secondName = htmlspecialchars($_POST['second_name'] ?? "");
+$middleName = htmlspecialchars($_POST['middle_name'] ?? "");
+$dateOfBirth = htmlspecialchars($_POST['date_of_birth'] ?? "");
+$email = htmlspecialchars($_POST['email'] ?? "");
+$password = htmlspecialchars($_POST['password'] ?? "");
 
 if (
     empty($firstName)
     || empty($secondName)
     || empty($middleName)
     || empty($dateOfBirth)
+    || empty($email)
     || empty($password)
 ) {
     die("Заполните пустое поле! Лол.");
 }
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die("Вы ввели некорректный адрес почты!");
+}
 $sql = <<<SQL
-INSERT INTO users (first_name, second_name, middle_name, date_of_birth, password) 
-    VALUES (:first_name, :second_name, :middle_name, :date_of_birth, :password) 
+INSERT INTO users (first_name, second_name, middle_name, date_of_birth, email, password) 
+    VALUES (:first_name, :second_name, :middle_name, :date_of_birth, :email, :password) 
 SQL;
 
 $pdo = getDB();
 $stmt = $pdo->prepare($sql);
-$stmt->execute([$firstName, $secondName, $middleName, $dateOfBirth, $password]);
+$stmt->execute([$firstName, $secondName, $middleName, $dateOfBirth, $email, $password]);
 
 header("Location: /index.php");
